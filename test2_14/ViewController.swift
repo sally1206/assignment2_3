f0e3bffec291fe829f2913b6f1a936158c9d3a8d
Sally Tsai <sally.tsai@odyssey.com.tw>
2018-11-05 16:54:51 +0800

    Initial Commit

---
+++ test2_14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2_14
+//
+//  Created by Sally Tsai on 2018/11/5.
+//  Copyright © 2018 Sally Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
